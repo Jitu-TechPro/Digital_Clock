fc2dd29fcf0cb73712f770270e6560f5625f0ed7
Jitu-TechPro <subhash.bhalerao@outlook.com>
2022-05-06 10:12:04 +0530

    Last commit, changed bg to RED

--- digitalClock.py
+++ digitalClock.py
@@ -7,9 +7,9 @@ from tkinter import Tk, Label
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
-window.configure(bg="steelblue")
+window.configure(bg="red")
 
-label = Label(window, font=("Ariel Black",78,"bold"), bg="steelblue", fg="white")
+label = Label(window, font=("Ariel Black",78,"bold"), bg="red", fg="white")
 label.pack(pady=100) 
 
 def clock():
